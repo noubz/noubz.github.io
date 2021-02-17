27b2efc52051945e7b538427996b9c610eecaba9
Berg132 <64863565+Berg132@users.noreply.github.com>
2021-02-17 08:58:54 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.addEventListener('scroll', () => {
-	document.body.style.setProperty('--scroll',window.pageYOffset / (document.offsetHeight - window.innerHeight));
-}, false);
+var scroll = function() {
+	let img_height = document.getElementById("banner").offsetHeight;
+	let nav = document.getElementById("navbar");
+
+	if (window.pageYOffset <= img_height) {
+		nav.style.top = (img_height - window.pageYOffset + 7) + "px";
+	}
+	else { nav.style.top = "7px"}
+};
+window.addEventListener('scroll', scroll);
+
+var update = function() {
+	scroll();
+}
+window.addEventListener('resize', update)
