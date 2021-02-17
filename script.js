560f381db61dcf2ec3a0503f36f596c3af607edf
Berg132 <64863565+Berg132@users.noreply.github.com>
2021-02-17 22:00:37 +0100

    update
    icon

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// Alle Überschriften
-var nav_items = document.getElementsByClassName("navitem");
+// Alle Listen
+const nav_items = document.getElementsByClassName("navitem");
+const sections = document.getElementsByTagName("section");
 
+// Navbar-Scroll aktualisierung
 function scroll() {
 	// Navbar verschiebung
 	let img_height = document.getElementById("banner").offsetHeight;
 	let nav = document.getElementById("navbar");
 
 	if (window.pageYOffset <= img_height) {
-		nav.style.top = (img_height - window.pageYOffset + 7) + "px";
+		nav.style.top = (img_height - window.pageYOffset) + "px";
 	}
 	else { nav.style.top = "7px"}
 
 	// Sektion detection
-	for (var section of [document.getElementById("banner"), document.getElementById("Project"), document.getElementById("Info"), document.getElementById("Impressum")]) {
+	for (var section of sections) {
 		if (section.getBoundingClientRect().top < (window.innerHeight / 2) ) {
 			for (var item of nav_items) {
 				if (item.href.includes(section.id)) { item.classList.add('active'); }
